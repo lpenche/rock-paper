723b6fb98f7602a1f4449001715793fd1bb0472f
lpenche <luucss@hotmail.com>
2022-05-17 13:03:05 +0200

    Add Capitalize function
    Create capitalize function and add to all return statements on singleRound function so that string displayed is capitalized

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,13 @@
+// Capitalize function
+function capitalize (string) {
+    let firstLetter = string.slice(0,1)
+    let rest = string.slice(1)
+    let stringOne = firstLetter.toUpperCase()
+    let stringTwo = rest.toLowerCase()
+    let result = stringOne + stringTwo
+    return result
+}
+
 // Generates random pick bewteen rock,paper and scissors and returns pick
 function computerPlay () {
     let randomNumber = Math.round(Math.random()*(3-1)) + 1;
@@ -13,29 +23,28 @@ function computerPlay () {
     }
     return selection
 }
-// Plays single round taking computerPlay() output and player selection
 
+// Plays single round taking computerPlay() output and player selection
 function singleRound (computerPlay, playerSelection) {
     playerSelection = playerSelection.toLowerCase() // Makes player selection non case sensitive.
     
     if (computerPlay === playerSelection) {
-        return `Tied, ${computerPlay} is equal to ${playerSelection}` 
+        return `Tied, ${capitalize(computerPlay)} is equal to ${capitalize(playerSelection)}` 
     } else if (computerPlay === "rock" && playerSelection === "paper") {
-        return `You win, ${playerSelection} beats ${computerPlay}` 
+        return `You win, ${capitalize(playerSelection)} beats ${capitalize(computerPlay)}` 
     } else if (computerPlay === "rock" && playerSelection === "scissors") {
-        return `You lose, ${computerPlay} beats ${playerSelection}`
+        return `You lose, ${capitalize(computerPlay)} beats ${capitalize(playerSelection)}`
     } else if (computerPlay === "paper" && playerSelection === "rock") {
-        return `You lose, ${computerPlay} beats ${playerSelection}`
+        return `You lose, ${capitalize(computerPlay)} beats ${capitalize(playerSelection)}`
     } else if (computerPlay === "paper" && playerSelection === "scissors") {
-        return `You win, ${playerSelection} beats ${computerPlay}`
+        return `You win, ${capitalize(playerSelection)} beats ${capitalize(computerPlay)}`
     } else if (computerPlay === "scissors" && playerSelection === "rock") {
-        return `You win, ${playerSelection} beats ${computerPlay}`
+        return `You win, ${capitalize(playerSelection)} beats ${capitalize(computerPlay)}`
     } else if (computerPlay === "scissors" && playerSelection === "paper") {
-        return `You lose, ${computerPlay} beats ${playerSelection}`
+        return `You lose, ${capitalize(computerPlay)} beats ${capitalize(playerSelection)}`
     } else {
         return "Coding error"
     }
 }
 
-console.log(computerPlay())
-console.log(singleRound(computerPlay(), "ROCK"))
+console.log(singleRound(computerPlay(), "RoCK"))
